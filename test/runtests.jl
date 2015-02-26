e3c031f8271882c77b6d683d6dd5e7d84974abf7
jake bolewski <jakebolewski@gmail.com>
2015-02-26 01:32:57 -0500

    add darray tests from Base

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
-using DistributedArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+n = min(8, CPU_CORES)
+n > 1 && addprocs(n; exeflags=`--check-bounds=yes`)
+blas_set_num_threads(1)
+
+@everywhere using DistrutedArrays
+
+include("darray.jl")
+
