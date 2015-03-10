e64d94e0d6f86239299785a1b743d0917cc81398
Jake Bolewski <jakebolewski@gmail.com>
2015-03-10 14:33:03 -0400

    refactor tests to use FactCheck

--- test/darray.jl
+++ test/darray.jl
@@ -1,83 +1,129 @@
+const MYID = myid()
+const OTHERIDS = filter(id-> id != MYID, procs())[rand(1:(nprocs()-1))]
 
-srand(123)
+facts("test DArray equality") do
+    D = drand((200,200), [MYID, OTHERIDS])
+    DC = copy(D)
 
-const id_me = myid()
-const id_other = filter(x -> x != id_me, procs())[rand(1:(nprocs()-1))]
+    context("test isequal(::Array, ::DArray)") do
+        @fact D == DC => true
+    end
 
-d = drand((200,200), [id_me, id_other])
-dc = copy(d)
-
-@test d == dc # Should be identical
-@spawnat id_other localpart(dc)[1] = 0
-@test fetch(@spawnat id_other localpart(d)[1] != 0) # but not point to the same memory
-
-s = convert(Matrix{Float64}, d[1:150, 1:150])
-a = convert(Matrix{Float64}, d)
-@test a[1:150,1:150] == s
-
-@test fetch(@spawnat id_me localpart(d)[1,1]) == d[1,1]
-@test fetch(@spawnat id_other localpart(d)[1,1]) == d[1,101]
-
-d = DArray(I->fill(myid(), map(length,I)), (10,10), [id_me, id_other])
-d2 = map(x->1, d)
-@test reduce(+, d2) == 100
-
-@test reduce(+, d) == ((50*id_me) + (50*id_other))
-map!(x->1, d)
-@test reduce(+, d) == 100
-
-let a = randn(10,10)
-    da = distribute(a)
-    @test scale!(da, 2) == scale!(a, 2)
-end
-
-# Test mapreduce on DArrays
-let
-    # Test that it is functionally equivalent to the standard method
-    for _ = 1:25, f = [x -> 2x, x -> x^2, x -> x^2 + 2x - 1], opt = [+, *]
-        n = rand(2:50)
-        arr = rand(1:100, n)
-        darr = distribute(arr)
-        @test mapreduce(f, opt, arr) == mapreduce(f, opt, darr)
+    context("test copy(::DArray) does a copy of each localpart") do
+        @spawnat OTHERIDS localpart(DC)[1] = 0
+        @fact fetch(@spawnat OTHERIDS localpart(D)[1] != 0) => true
     end
 end
 
-# Test mapreducedim on DArrays
-@test mapreducedim(t -> t*t, +, d2, 1) == mapreducedim(t -> t*t, +, convert(Array, d2), 1)
-@test mapreducedim(t -> t*t, +, d2, 2) == mapreducedim(t -> t*t, +, convert(Array, d2), 2)
-@test mapreducedim(t -> t*t, +, d2, (1,2)) == mapreducedim(t -> t*t, +, convert(Array, d2), (1,2))
-dims = (20,20,20)
 
-d = drandn(dims)
-da = convert(Array, d)
-for dms in (1, 2, 3, (1,2), (1,3), (2,3), (1,2,3))
-    @test_approx_eq mapreducedim(t -> t*t, +, d, dms) mapreducedim(t -> t*t, +, da, dms)
-    @test_approx_eq mapreducedim(t -> t*t, +, d, dms, 1.0) mapreducedim(t -> t*t, +, da, dms, 1.0)
+facts("test DArray / Array conversion") do
+    D = drand((200,200), [MYID, OTHERIDS])
 
-    @test_approx_eq reducedim(*, d, dms) reducedim(*, da, dms)
-    @test_approx_eq reducedim(*, d, dms, 2.0) reducedim(*, da, dms, 2.0)
+    context("test convert(::Array, ::(Sub)DArray)") do
+        S = convert(Matrix{Float64}, D[1:150, 1:150])
+        A = convert(Matrix{Float64}, D)
 
-    # statistical function (works generically through calls to mapreducedim!, i.e. not implemented specifically for DArrays)
-    @test_approx_eq mean(d, dms) mean(da, dms)
-    # @test_approx_eq std(d, dms) std(da, dms) Requires centralize_sumabs2! for DArrays
+        @fact A[1:150,1:150] => S
+        @fact fetch(@spawnat MYID localpart(D)[1,1]) => D[1,1]
+        @fact fetch(@spawnat OTHERIDS localpart(D)[1,1]) => D[1,101]
+    end
 end
 
-let A = randn(100,100), DA = distribute(A)
-    #@test_throws BoundsError sum(DA,-1)
-    @test sum(A,0) == sum(DA,0)
-    @test_approx_eq_eps sum(A) sum(DA) 1e-12
-    @test_approx_eq_eps sum(A,1) sum(DA,1) 1e-12
-    @test_approx_eq_eps sum(A,2) sum(DA,2) 1e-12
-    @test_approx_eq_eps sum(A,3) sum(DA,3) 1e-12
+facts("test DArray reduce") do
+    D = DArray(id->fill(myid(), map(length,id)), (10,10), [MYID, OTHERIDS])
 
-    @test_throws BoundsError size(DA, 0)
-    @test size(A,1) == size(DA,1)
-    @test size(A,2) == size(DA,2)
-    @test size(A,3) == size(DA,3)
+    context("test reduce") do
+        @fact reduce(+, D) => ((50*MYID) + (50*OTHERIDS))
+    end
+
+    context("test map / reduce") do
+        D2 = map(x->1, D)
+        @fact reduce(+, D2) => 100
+    end
+
+    context("test map! / reduce") do
+        map!(x->1, D)
+        @fact reduce(+, D) => 100
+    end
+end
+
+facts("test scale") do
+    A = randn(100,100)
+    DA = distribute(A)
+    @fact scale!(DA, 2) => scale!(A, 2)
+end
+
+facts("test mapreduce on DArrays") do
+    for _ = 1:25, f = [x -> 2x, x -> x^2, x -> x^2 + 2x - 1], opt = [+, *]
+        A = rand(1:100, rand(2:50))
+        DA = distribute(A)
+        @fact abs(mapreduce(f, opt, A) - mapreduce(f, opt, DA)) < 1e-12 => true
+    end
+end
+
+facts("test mapreducedim on DArrays") do
+    D = DArray(I->fill(myid(), map(length,I)), (73,73), [MYID, OTHERIDS])
+    D2 = map(x->1, D)
+    @fact mapreducedim(t -> t*t, +, D2, 1) => mapreducedim(t -> t*t, +, convert(Array, D2), 1)
+    @fact mapreducedim(t -> t*t, +, D2, 2) => mapreducedim(t -> t*t, +, convert(Array, D2), 2)
+    @fact mapreducedim(t -> t*t, +, D2, (1,2)) => mapreducedim(t -> t*t, +, convert(Array, D2), (1,2))
+end
+
+facts("test mapreducdim, reducedim on DArrays") do
+    dims = (20,20,20)
+    DA = drandn(dims)
+    A = convert(Array, DA)
+
+    for dms in (1, 2, 3, (1,2), (1,3), (2,3), (1,2,3))
+        @fact mapreducedim(t -> t*t, +, DA, dms) => roughly(mapreducedim(t -> t*t, +, A, dms))
+        @fact mapreducedim(t -> t*t, +, DA, dms, 1.0) => roughly(mapreducedim(t -> t*t, +, A, dms, 1.0))
+
+        @fact reducedim(*, DA, dms) => roughly(reducedim(*, A, dms))
+        @fact reducedim(*, DA, dms, 2.0) => roughly(reducedim(*, A, dms, 2.0))
+    end
+end
+
+
+#=
+# statistical function (works generically through calls to mapreducedim!,
+i.e. not implemented specifically for DArrays)
+@test_approx_eq mean(d, dms) mean(da, dms)
+# @test_approx_eq std(d, dms) std(da, dms) Requires centralize_sumabs2! for DArrays
+end
+=#
+
+facts("test sum on DArrays") do
+    A = randn(100,100)
+    DA = distribute(A)
+
+    #@test_throws BoundsError sum(DA,-1)
+    #@fact sum(DA,0) => sum(DA,0)
+    @fact sum(DA) => roughly(sum(A), atol=1e-12)
+    @fact sum(DA,1) => roughly(sum(A,1), atol=1e-12)
+    @fact sum(DA,2) => roughly(sum(A,2), atol=1e-12)
+    @fact sum(DA,3) => roughly(sum(A,3), atol=1e-12)
+end
+
+facts("test size on DArrays") do
+    A = randn(100,100)
+    DA = distribute(A)
+
+    @fact_throws size(DA, 0) # BoundsError
+    @fact size(DA,1) => size(A,1)
+    @fact size(DA,2) => size(A,2)
+    @fact size(DA,3) => size(A,3)
 end
 
 # test length / endof
-let A = randn(100,100), DA = distribute(A)
-    @test length(A) == length(DA)
-    @test endof(A) == endof(DA)
+facts("test collections API") do
+    A = randn(23,23)
+    DA = distribute(A)
+
+    context("test length") do
+        @fact length(DA) => length(A)
+    end
+
+    context("test endof") do
+        @fact endof(DA) => endof(A)
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,17 @@
-using Base.Test
+using FactCheck
 
 # add at least 3 worker processes
 if nworkers() < 3
     n = max(3, min(8, CPU_CORES))
     addprocs(n; exeflags=`--check-bounds=yes`)
 end
-
 @assert nprocs() > 3
 @assert nworkers() >= 3
 
 @everywhere using DistributedArrays
 @everywhere blas_set_num_threads(1)
+@everywhere srand(123 + myid())
 
 include("darray.jl")
+
+FactCheck.exitstatus()
