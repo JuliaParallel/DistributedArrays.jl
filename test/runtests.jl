7cc66201655af034759aea149690937a90ed68cb
Jake Bolewski <jakebolewski@gmail.com>
2015-03-10 11:20:45 -0400

    make sure all processes start blas with 1 thread

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Base.Test
 
 n = min(8, CPU_CORES)
 n > 1 && addprocs(n; exeflags=`--check-bounds=yes`)
-blas_set_num_threads(1)
 
 @everywhere using DistributedArrays
+@everywhere blas_set_num_threads(1)
 
 include("darray.jl")
