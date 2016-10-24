3f5e683e8205204a946c2dfd871a42523ec420c9
nschickel <schickel@mit.edu>
2016-10-24 12:55:17 -0400

    Working cumsum function for DArrays
    Needs updates for flexibility

--- src/core.jl
+++ src/core.jl
@@ -714,16 +714,28 @@ Base.fill!(A::DArray, x) = begin
 end
 
 # new work #
-function Base.cumsum(A::DArray)
-    out1 = let ds = ds
-        [remotecall(()->cumsum(localpart(ds),2)[end],p) for p = workers()]
-    end
-    out2 = [0 ; cumsum(fetch.(out1))]
-    out3 = Future[]
-    for i = 1:length(workers())
-        p = workers()[i]
-        out2i = out2[i]
-        push!(out3,remotecall(()->+(out2i,localpart(ds)),p))
-    end
-    return DArray(out3)
- end
+
+function star(f1,f2,f3,args::DistributedArrays.DArray...)
+        let ds = ds
+            out1 = [remotecall( ()-> f1( DistributedArrays.localpart(ds),2 )[end],p ) for p = workers()]
+
+            out2 = [0; f2(map(fetch,out1))]
+
+            out3 = Future[]
+                    for i = 1:length(workers())
+                        p = workers()[i]
+                        out2i = out2[i]
+                        push!(out3,remotecall(()->f3(out2i,f1( DistributedArrays.localpart(ds),2) ),p))
+                    end
+
+            tmp = [out3[1] out3[2] out3[3] out3[4]]
+
+            return DArray(tmp)
+        end
+end
+
+import Base.cumsum
+
+function cumsum(args::DistributedArrays.DArray...)
+    return star(cumsum,cumsum,+,args)
+end
