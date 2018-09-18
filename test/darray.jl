97b4779e692177c30140ba2208d10e48adb573f9
Joe Tomlinson <jxt732@psu.edu>
2018-09-18 07:23:30 -0400

    Fixed Missing End

--- test/darray.jl
+++ test/darray.jl
@@ -58,10 +58,11 @@ using SparseArrays: nnz
     end
     
     @testset "Consistent Uneven Distribution issue #166" begin
-        DA = drand((7,), procs()[1:3], 3)
+        DA = drand((7,), [MYID, OTHERIDS[1:2]])
         @test fetch(@spawnat MYID length(localpart(DA)) == 3)
-        @test fetch(@spawnat OTHERIDS length(localpart(DA)) == 2)
+        @test fetch(@spawnat OTHERIDS[1:2] length(localpart(DA)) == 2)
         close(DA)
+    end
 end
 
 check_leaks()
